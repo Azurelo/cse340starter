1f06c8a53dc19ef0fa982d6ffeafc9a9dcc01f60
Justus <galaxycheesemc@gmail.com>
2025-04-01 16:19:59 -0700

    reviews added

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -49,6 +49,16 @@ CREATE TABLE IF NOT EXISTS public.account
     CONSTRAINT account_pkey PRIMARY KEY (account_id)
 );
 
+CREATE TABLE IF NOT EXISTS public.review (
+    review_id SERIAL PRIMARY KEY,
+    review_text TEXT NOT NULL,
+    review_date TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
+    inv_id INTEGER NOT NULL,
+    account_id INTEGER NOT NULL,
+    CONSTRAINT fk_inventory FOREIGN KEY (inv_id) REFERENCES inventory(inv_id) ON DELETE CASCADE,
+    CONSTRAINT fk_account FOREIGN KEY (account_id) REFERENCES account(account_id) ON DELETE CASCADE
+);
+
 -- Data for table `classification`
 INSERT INTO public.classification (classification_name)
 VALUES ('Custom'),
