9bc4680a30a52c5e0f777bc9433d2a3439fb4f98
Justus <galaxycheesemc@gmail.com>
2025-04-02 19:38:03 -0700

    error handling for reviews

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const reviewController = require('../controllers/reviewController');
-
+const utilities = require('../utilities/account-validation');
 //Routes for /reviews
-router.post('/add', reviewController.addReview);
-router.get('/inventory/:inv_id', reviewController.displayReviews);
-router.get('/edit/:review_id', reviewController.editReviewForm);
-router.post('/edit/:review_id', reviewController.editReview);
-router.post('/delete/:review_id', reviewController.deleteReview);
+router.post('/add', utilities.handleErrors(reviewController.addReview));
+router.get('/inventory/:inv_id', utilities.handleErrors(reviewController.displayReviews));
+router.get('/edit/:review_id', utilities.handleErrors(reviewController.editReviewForm));
+router.post('/edit/:review_id', utilities.handleErrors(reviewController.editReview));
+router.post('/delete/:review_id', utilities.handleErrors(reviewController.deleteReview));
 
 module.exports = router;
